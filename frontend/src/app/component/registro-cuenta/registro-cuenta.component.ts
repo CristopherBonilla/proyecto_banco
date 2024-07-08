3162f05e80d6ea90adb21e4c0f596908afe8e752
Domenica Rodriguez <112053642+DomeLiz@users.noreply.github.com>
2024-07-07 21:50:34 -0500

    Componentes para el frontend

---
+++ frontend/src/app/component/registro-cuenta/registro-cuenta.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-registro-cuenta',
+  standalone: true,
+  imports: [],
+  templateUrl: './registro-cuenta.component.html',
+  styleUrl: './registro-cuenta.component.css'
+})
+export class RegistroCuentaComponent {
+
+}
---
+++ frontend/src/app/component/suspencion-cliente/suspencion-cliente.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-suspencion-cliente',
+  standalone: true,
+  imports: [],
+  templateUrl: './suspencion-cliente.component.html',
+  styleUrl: './suspencion-cliente.component.css'
+})
+export class SuspencionClienteComponent {
+
+}
