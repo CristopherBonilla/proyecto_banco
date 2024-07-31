42b2938a165dd94118baf99c30ede2ac49cc1787
Domenica Rodriguez <112053642+DomeLiz@users.noreply.github.com>
2024-07-31 01:29:32 -0500

    boton de reenvio de codigo en trasferrencia

--- frontend/src/app/Components/transferencias-internas/transferencias-internas.component.ts
+++ frontend/src/app/Components/transferencias-internas/transferencias-internas.component.ts
@@ -14,9 +14,10 @@ import { CuentaService } from 'src/app/services/cuenta/cuenta.service';
 export class TransferenciasInternasComponent {
   cuentas: Cuenta[] = [];
   numeroCuentas: string[] = [];
-  correo: String = "";
+  correo: string = ""; // Cambiar a tipo primitivo string
   codigoValido: boolean = false;
   cuentaValida: boolean = false; // Estado de validez de la cuenta de destino
+  codigoOTP: string = ""; 
 
   constructor(
     private fb: FormBuilder,
@@ -33,23 +34,23 @@ export class TransferenciasInternasComponent {
 
   extraerDatos() {
     const objeto = history.state.transferenciaObj;
-    var cedula = objeto.cedula;
+    const cedula = objeto.cedula;
     this.cuentas = objeto.cuentas;
     const nombre = { cedula: cedula };
     this._clienteService.obtenerCliente(nombre).subscribe(data => {
       this.correo = data.correo_electronico;
-      var nombres = data.nombres.toString();
-      var apellidos = data.apellidos.toString();
-      var text = document.getElementById('nombre-cliente');
+      const nombres = data.nombres.toString();
+      const apellidos = data.apellidos.toString();
+      const text = document.getElementById('nombre-cliente');
       text!.innerHTML = nombres + ' ' + apellidos;
     })
-    for (var i = 0; i < this.cuentas.length; i++) {
+    for (let i = 0; i < this.cuentas.length; i++) {
       this.numeroCuentas[i] = this.cuentas[i].tipo_cuenta + " " + this.cuentas[i].numero_cuenta + " $" + this.cuentas[i].monto_inicial;
       // cargar cuentas en el combo box
     }
-    var select = document.getElementById("cuentasOrigen");
-    for (var i = 0; i < this.numeroCuentas.length; i++) {
-      var option = document.createElement("option");
+    const select = document.getElementById("cuentasOrigen") as HTMLSelectElement;
+    for (let i = 0; i < this.numeroCuentas.length; i++) {
+      const option = document.createElement("option");
       option.innerHTML = this.numeroCuentas[i];
       select!.appendChild(option);
     }
@@ -70,37 +71,36 @@ export class TransferenciasInternasComponent {
   }
 
   validarCuenta() {
-    var cuenta = document.getElementById("cuentaDestino-campo") as HTMLInputElement;
+    const cuenta = document.getElementById("cuentaDestino-campo") as HTMLInputElement;
     const cuentaObj = { numero_cuenta: cuenta.value };
-    var texto = document.getElementById("textCuenta");
+    const texto = document.getElementById("textCuenta");
     
     this._CuentaService.obtenerCuenta(cuentaObj).subscribe(
       data => {
-        if (data && cuenta) { // Verifica si la cuenta tiene una cédula válida
-          let cuenta = <Cuenta>data;
-          const cedulaObj = { cedula: cuenta.cedula };
+        if (data) { // Verifica si la cuenta es valida
+          const cuentaData = data as Cuenta; // Asegúrate de que el tipo sea Cuenta
+          const cedulaObj = { cedula: cuentaData.cedula };
           
-          this._clienteService.obtenerCliente(cedulaObj).subscribe(data => {
-            texto!.innerHTML = "Cuenta encontrada. Esta cuenta le pertenece a: " + data.nombres + " " + data.apellidos;
+          this._clienteService.obtenerCliente(cedulaObj).subscribe(clienteData => {
+            texto!.innerHTML = "Cuenta encontrada. Esta cuenta le pertenece a: " + clienteData.nombres + " " + clienteData.apellidos;
             this.cuentaValida = true; // Habilitar el botón de "Enviar código" si la cuenta es válida
           });
         } else {
           // Manejar el caso en que la cuenta no tenga una cédula válida
           this.cuentaValida = false; // Deshabilitar el botón de "Enviar código"
-          texto!.innerHTML = "Cuenta no encontrada o error en la solicitud.";
+          texto!.innerHTML = "Cuenta no encontrada";
           this.toastr.error('Cuenta de destino no válida.', 'Error');
         }
       },
       error => {
         // Manejar el caso en que la cuenta no existe o hay un error en la solicitud
         this.cuentaValida = false; // Deshabilitar el botón de "Enviar código"
-        texto!.innerHTML = "Cuenta no encontrada o error en la solicitud.";
+        texto!.innerHTML = "Cuenta no encontrada ";
         this.toastr.error('Cuenta de destino no válida.', 'Error');
       }
     );
   }
   
-
   transferir(){
     var cuenta1:String="";
     var monto = document.getElementById("monto-campo") as HTMLInputElement;
@@ -119,13 +119,14 @@ export class TransferenciasInternasComponent {
       this.toastr.success('La transferencia se realizó con éxito.', 'Transferencia exitosa');
       setTimeout(() => {
         this.menu();
-      }, 1000); // Espera 2 segundos antes de redirigir
+      }, 1000); 
     });
     const resumen = { cuenta1: cuenta1, cuenta2: cuenta2.value, monto: monto.value, descripcion: descripcion.value, correo: this.correo };
     this._clienteService.resumen(resumen).subscribe(data => {
       console.log(data);
     })
-  }     
+  } 
+             
 
   otp() {
     // Deshabilitar el botón de correo
@@ -133,29 +134,39 @@ export class TransferenciasInternasComponent {
     document.getElementById('otp')!.style.display = 'block';
     // Enviar correo
     this.confirmarCorreo(this.correo);
+    this.toastr.success('Código enviado correctamente al correo', 'Éxito');
+  }
+  enviarCodigo() {
+    this.toastr.success('Código enviado correctamente al correo', 'Éxito');
+    this.confirmarCorreo(this.correo);
+    
   }
 
-  confirmarCorreo(email: String) {
-    var codigo = "";
-    const correo = { correo: email }
+  reenviarCodigo(event: Event) {
+    event.preventDefault(); // Prevenir comportamiento por defecto del enlace
+    this.enviarCodigo();
+  }
+
+  confirmarCorreo(email: string) {
+    let codigo = "";
+    const correo = { correo: email };
     this._clienteService.confirmarTransferencia(correo).subscribe(
       data => {
         codigo = data.toString();
-        let patron = "^" + codigo + "$";
-        var campo = document.getElementById('otp-campo');
+        const patron = "^" + codigo + "$";
+        const campo = document.getElementById('otp-campo');
         campo!.addEventListener('keyup', () => {
-          var text = document.getElementById('text');
-          var otp = document.getElementById("otp-campo") as HTMLInputElement;
+          const text = document.getElementById('text');
+          const otp = document.getElementById("otp-campo") as HTMLInputElement;
           if (otp.value.match(patron) == null) {
             text!.innerHTML = "Codigo invalido";
             this.codigoValido = false;
-          
           } else {
             text!.innerHTML = "Codigo valido";
             this.codigoValido = true;
           }
-        })
+        });
       }
-    )
+    );
   }
 }
