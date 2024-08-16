5cb203da808e9b356dfe576bd97b6d0f1fa2f2f4
NelsonCasa12 <nelsoncasa2017@gmail.com>
2024-08-16 00:01:26 -0500

    cambio en test
    cambio en test

--- frontend/src/app/Components/nuevas-credenciales/nuevas-credenciales.component.spec.ts
+++ frontend/src/app/Components/nuevas-credenciales/nuevas-credenciales.component.spec.ts
@@ -1,4 +1,40 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ReactiveFormsModule } from '@angular/forms';
+import { ToastrModule, ToastrService } from 'ngx-toastr';
+
+import { NuevasCredencialesComponent } from './nuevas-credenciales.component';
+import { UsuarioService } from '../../services/usuario.service'; // Asegúrate de que la ruta sea correcta
+
+describe('NuevasCredencialesComponent', () => {
+  let component: NuevasCredencialesComponent;
+  let fixture: ComponentFixture<NuevasCredencialesComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ NuevasCredencialesComponent ],
+      imports: [
+        HttpClientModule,
+        BrowserAnimationsModule, // Necesario para Toastr
+        ToastrModule.forRoot(),  // Configuración global para Toastr
+        ReactiveFormsModule      // Importa ReactiveFormsModule para el uso de formGroup
+      ],
+      providers: [ UsuarioService, ToastrService ] // Proveedor de ToastrService
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(NuevasCredencialesComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
+
+/*import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { NuevasCredencialesComponent } from './nuevas-credenciales.component';
 
@@ -20,4 +56,4 @@ describe('NuevasCredencialesComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
-});
+});*/
--- frontend/src/app/Components/pregunta-seguridad/pregunta-seguridad.component.spec.ts
+++ frontend/src/app/Components/pregunta-seguridad/pregunta-seguridad.component.spec.ts
@@ -1,6 +1,11 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { RouterTestingModule } from '@angular/router/testing';
 import { PreguntaSeguridadComponent } from './pregunta-seguridad.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { ToastrModule } from 'ngx-toastr';
+import { UsuarioService } from 'src/app/services/usuario/usuarios.service';
+import { ClienteService } from 'src/app/services/cliente/cliente.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 describe('PreguntaSeguridadComponent', () => {
   let component: PreguntaSeguridadComponent;
@@ -8,10 +13,22 @@ describe('PreguntaSeguridadComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ PreguntaSeguridadComponent ]
+      imports: [
+        RouterTestingModule,
+        ReactiveFormsModule,
+        HttpClientTestingModule,
+        ToastrModule.forRoot()  // Importa el ToastrModule con una configuración básica
+      ],
+      declarations: [PreguntaSeguridadComponent],
+      providers: [
+        UsuarioService,
+        ClienteService
+      ]
     })
     .compileComponents();
+  });
 
+  beforeEach(() => {
     fixture = TestBed.createComponent(PreguntaSeguridadComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
