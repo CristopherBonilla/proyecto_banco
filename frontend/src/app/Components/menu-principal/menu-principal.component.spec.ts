70c927b12daed699f6ff9a0804ea638e3f40f32f
Anthony.Reinoso <pitufin26004@hotmail.com>
2024-08-16 01:11:34 -0500

    falta transferencias

--- frontend/src/app/Components/menu-principal/menu-principal.component.spec.ts
+++ frontend/src/app/Components/menu-principal/menu-principal.component.spec.ts
@@ -3,33 +3,47 @@ import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { of } from 'rxjs';
 
 import { MenuPrincipalComponent } from './menu-principal.component';
-import { ClienteService } from '../../services/cliente/cliente.service';
+import { ClienteService } from 'src/app/services/cliente/cliente.service';
+import { CuentaService } from 'src/app/services/cuenta/cuenta.service';
+import { HistoryService } from 'src/app/services/history.service';
 
 class MockClienteService {
   obtenerCliente(cedula: object) {
-    // Simulación de la respuesta del servidor
-    return of({ cedula: '1234567890' });
+    return of({ nombres: 'Juan', apellidos: 'Pérez' });
+  }
+}
+
+class MockCuentaService {
+  getCuentaByCI(cuenta: object) {
+    return of([{ tipo_cuenta: '10' }, { tipo_cuenta: '20' }]);
+  }
+}
+
+class MockHistoryService {
+  getState() {
+    return { cedula: { cedula: '1234567890' } };
   }
 }
 
 describe('MenuPrincipalComponent', () => {
   let component: MenuPrincipalComponent;
   let fixture: ComponentFixture<MenuPrincipalComponent>;
-  let clienteService: ClienteService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [HttpClientTestingModule],
-      declarations: [ MenuPrincipalComponent ],
+      declarations: [MenuPrincipalComponent],
       providers: [
-        { provide: ClienteService, useClass: MockClienteService }
+        { provide: ClienteService, useClass: MockClienteService },
+        { provide: CuentaService, useClass: MockCuentaService },
+        { provide: HistoryService, useClass: MockHistoryService }
       ]
-    })
-    .compileComponents();
+    }).compileComponents();
+  });
 
+  beforeEach(() => {
     fixture = TestBed.createComponent(MenuPrincipalComponent);
     component = fixture.componentInstance;
-    clienteService = TestBed.inject(ClienteService);
     fixture.detectChanges();
   });
 
@@ -37,7 +51,14 @@ describe('MenuPrincipalComponent', () => {
     expect(component).toBeTruthy();
   });
 
+  it('should handle client and account data', () => {
+    component.ngOnInit();
+    expect(component.listCuentas.length).toBe(2);
+    expect(component.listCuentas[0].tipo_cuenta).toBe('Cuenta de Ahorros');
+    expect(component.listCuentas[1].tipo_cuenta).toBe('Cuenta Corriente');
+  });
 });
+
 
 /*import { ComponentFixture, TestBed } from '@angular/core/testing';
 
--- frontend/src/app/Components/registro-cliente/registro-cliente.component.spec.ts
+++ frontend/src/app/Components/registro-cliente/registro-cliente.component.spec.ts
@@ -1,4 +1,36 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { ReactiveFormsModule } from '@angular/forms';
+import { ToastrModule } from 'ngx-toastr'; // Importa el módulo de Toastr
+import { RegistroClienteComponent } from './registro-cliente.component';
+
+describe('RegistroClienteComponent', () => {
+  let component: RegistroClienteComponent;
+  let fixture: ComponentFixture<RegistroClienteComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [
+        HttpClientTestingModule, // Añade el módulo de pruebas HTTP
+        ReactiveFormsModule,     // Añade el módulo de formularios reactivos, si es necesario
+        ToastrModule.forRoot()   // Añade el módulo de Toastr aquí
+      ],
+      declarations: [ RegistroClienteComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(RegistroClienteComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
+
+
+/*import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { RegistroClienteComponent } from './registro-cliente.component';
 
@@ -21,3 +53,4 @@ describe('RegistroClienteComponent', () => {
     expect(component).toBeTruthy();
   });
 });
+*/
