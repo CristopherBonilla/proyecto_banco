ef1e01262a4dd3cd33b84e63acc5fd992ebe834d
Anthony.Reinoso <pitufin26004@hotmail.com>
2024-08-16 00:11:07 -0500

    menu principal y appcomponent

--- frontend/src/app/Components/menu-principal/menu-principal.component.spec.ts
+++ frontend/src/app/Components/menu-principal/menu-principal.component.spec.ts
@@ -1,4 +1,45 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { of } from 'rxjs';
+
+import { MenuPrincipalComponent } from './menu-principal.component';
+import { ClienteService } from '../../services/cliente/cliente.service';
+
+class MockClienteService {
+  obtenerCliente(cedula: object) {
+    // Simulación de la respuesta del servidor
+    return of({ cedula: '1234567890' });
+  }
+}
+
+describe('MenuPrincipalComponent', () => {
+  let component: MenuPrincipalComponent;
+  let fixture: ComponentFixture<MenuPrincipalComponent>;
+  let clienteService: ClienteService;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      declarations: [ MenuPrincipalComponent ],
+      providers: [
+        { provide: ClienteService, useClass: MockClienteService }
+      ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(MenuPrincipalComponent);
+    component = fixture.componentInstance;
+    clienteService = TestBed.inject(ClienteService);
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+});
+
+/*import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { MenuPrincipalComponent } from './menu-principal.component';
 
@@ -21,3 +62,4 @@ describe('MenuPrincipalComponent', () => {
     expect(component).toBeTruthy();
   });
 });
+*/
