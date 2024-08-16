0f406f16ce4d237a7b8946d078aecefef95f2aa4
NelsonCasa12 <nelsoncasa2017@gmail.com>
2024-08-16 00:27:07 -0500

    cambios
    cambios

--- frontend/src/app/Components/menu-principal/menu-principal.component.spec.ts
+++ frontend/src/app/Components/menu-principal/menu-principal.component.spec.ts
@@ -1,4 +1,32 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MenuPrincipalComponent } from './menu-principal.component';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { ClienteService } from 'src/app/services/cliente/cliente.service';
+
+describe('MenuPrincipalComponent', () => {
+  let component: MenuPrincipalComponent;
+  let fixture: ComponentFixture<MenuPrincipalComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ MenuPrincipalComponent ],
+      imports: [ HttpClientTestingModule ], // Importamos HttpClientTestingModule
+      providers: [ ClienteService ] // Proveedor del ClienteService
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(MenuPrincipalComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
+
+
+/*import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { MenuPrincipalComponent } from './menu-principal.component';
 
@@ -20,4 +48,4 @@ describe('MenuPrincipalComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
-});
+});*/
